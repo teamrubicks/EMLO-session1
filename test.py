893af0cc5104ae08b0113a519c9e0d4587afc098
medonmol <gairola.abhijit.ctrl@gmail.com>
2021-09-24 13:54:29 +0530

    modified tests

--- test.py
+++ test.py
@@ -3,27 +3,35 @@ import pandas as pd
 import os
 
 
-
-
 def test_data_existance():
     files = os.listdir()
-    assert 'data' not in files
+    assert "data" not in files
+    assert "data.zip" not in files
+    assert "bottleneck_features_train.npy" not in files
+    assert "bottleneck_features_train.npy.dvc" in files
+    assert "bottleneck_features_valid.npy" not in files
+    assert "bottleneck_features_valid.npy.dvc" in files
+
 
 def test_model_existance():
     files = os.listdir()
-    assert 'model.h5' not in files
+    assert "dognotdog.pt" not in files
+    assert "dognotdog.pt.dvc" in files
+
 
 def test_train_model_accuracy():
-    metrics = pd.read_csv('metrics.csv')
-    max_acc = metrics.loc[metrics.mode=='train','accuracy'].max()
-    assert max_acc>= .70
+    metrics = pd.read_csv("metrics.csv")
+    max_acc = metrics.loc[metrics.mode == "train", "accuracy"].max()
+    assert max_acc >= 0.70
+
 
 def test_train_notadog_accuracy():
-    metrics = pd.read_csv('metrics.csv')
-    max_acc = metrics.loc[metrics.mode=='train','class0_accuracy'].max()
-    assert max_acc>= .70
+    metrics = pd.read_csv("metrics.csv")
+    max_acc = metrics.loc[metrics.mode == "train", "class0_accuracy"].max()
+    assert max_acc >= 0.70
+
 
 def test_train_dog_accuracy():
-    metrics = pd.read_csv('metrics.csv')
-    max_acc = metrics.loc[metrics.mode=='train','class1_accuracy'].max()
-    assert max_acc>= .70
+    metrics = pd.read_csv("metrics.csv")
+    max_acc = metrics.loc[metrics.mode == "train", "class1_accuracy"].max()
+    assert max_acc >= 0.70
